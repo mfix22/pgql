34b59bc59e1c037309163a396dd749ddce502e31
Mike Fix <mrfix84@gmail.com>
2019-01-25 12:57:20 -0800

    migrate to Now 2.0

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const prettier = require('prettier')
-const { text, createError } = require('micro');
+const { text, send } = require('micro')
 
-module.exports = async req => {
-  if (req.method !== 'POST') return `
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return send(
+      res,
+      200,
+      `
 <pre>
 Please send your query/mutation as text in the body of a POST request
 
   Example:
     $ curl https://pgql.now.sh -X POST -d 'query { hello }'
 </pre>
-`
+    `
+    )
+  }
 
   let data
   try {
-    data = await text(req);
+    data = await text(req)
   } catch (e) {
-    console.log(e);
-    throw new Error ('Please send your query/mutation as text in the body of a POST request')
+    console.log(e)
+    throw new Error('Please send your query/mutation as text in the body of a POST request')
   }
 
   try {
-    return prettier.format(data, { parser: 'graphql' })
+    send(res, 200, prettier.format(data, { parser: 'graphql' }))
   } catch (e) {
-    throw createError(400, e)
+    send(res, 400, e.message)
   }
-};
+}
